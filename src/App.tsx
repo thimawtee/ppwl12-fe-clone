0c5d5ceaaaa214e97a32195fd0b4298c9ae80816
h1101241011-cloud <h1101241011@student.untan.ac.id>
2026-04-29 22:03:17 +0700

    menambahkna ask you

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Footer from "./components/Footer";
 import KeepWorking from './components/KeepWorking';
+import Askyou from './components/Askyou';
 function App() {
   return (
     <div className="min-h-screen bg-white text-notion-black">
       <Navbar />
       <Hero />
       <KeepWorking/>
+      <Askyou />
       <Footer />
     </div>
 )}
