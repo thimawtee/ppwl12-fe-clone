a02c36df208e43da73cbd0aa14eadc041ac50d46
Nadya1030 <h1101241030@student.untan.ac.id>
2026-04-29 20:25:16 +0700

    Bring All Your Fitur

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Footer from "./components/Footer";
+import BringAllYour from "./components/BringAllYour";
 
 function App() {
   return (
